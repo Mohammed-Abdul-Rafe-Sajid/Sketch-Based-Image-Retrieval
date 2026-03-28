6affec7f3a50a46b768212a7233b4ebb4eb18830
Mohammed Abdul Rafe Sajid <abdulrafesajid@gmail.com>
2026-03-28 22:55:23 +0530

    just added prev code

--- app.py
+++ app.py
@@ -85,5 +85,94 @@ if uploaded_file:
             st.write(f"Average Precision@5: {score:.2f}")
         except Exception:
             st.warning("Evaluation module not available in deployed version.")
+
+
+# import streamlit as st
+# import numpy as np
+# import cv2
+# import torch
+# import torchvision.transforms as transforms
+# from torchvision.models import resnet18, ResNet18_Weights
 # from torchvision.datasets import CIFAR10
+
+# # ---------------- MODEL ----------------
+# model = resnet18(weights=ResNet18_Weights.DEFAULT)
+# model = torch.nn.Sequential(*list(model.children())[:-1])
+# model.eval()
+
+# transform = transforms.Compose([
+#     transforms.ToPILImage(),
+#     transforms.Resize((224, 224)),
+#     transforms.ToTensor(),
+# ])
+
+# # ---------------- LOAD DATA ----------------
+# filenames = np.load("features/filenames.npy", allow_pickle=True)
+# embeddings = np.load("features/embeddings.npy")
+
+# # ---------------- FUNCTIONS ----------------
+# def extract_embedding(img):
+#     img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+#     img = transform(img).unsqueeze(0)
+
+#     with torch.no_grad():
+#         emb = model(img)
+
+#     return emb.view(-1).numpy()
+
+
+# def cosine_similarity(a, b):
+#     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+
+# def retrieve(query_img, top_k=5):
+#     query_emb = extract_embedding(query_img)
+#     sims = []
+
+#     for i, emb in enumerate(embeddings):
+#         sim = cosine_similarity(query_emb, emb)
+#         sims.append((filenames[i], sim))
+
+#     sims.sort(key=lambda x: x[1], reverse=True)
+#     return sims[:top_k]
+
+
+# # ---------------- UI ----------------
+# st.title("🖊️ Sketch-Based Image Retrieval")
+
+# uploaded_file = st.file_uploader(
+#     "Upload a sketch (or edge image)",
+#     type=["png", "jpg"]
+# )
+
+# if uploaded_file:
+#     # Read image
+#     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+#     img = cv2.imdecode(file_bytes, cv2.IMREAD_GRAYSCALE)
+
+#     st.subheader("Query Image")
+#     st.image(img, channels="GRAY")
+
+#     results = retrieve(img)
+
+#     st.subheader("Top Matches")
+#     cols = st.columns(len(results))
+
+#     for i, (file, score) in enumerate(results):
+#         idx = int(file.split("_")[1].split(".")[0])
+
 #         dataset = CIFAR10(root="data/raw", train=True, download=False)
+#         img, _ = dataset[idx]
+#         img = np.array(img)
+
+#         with cols[i]:
+#             st.image(img, caption=f"{score:.2f}", width=150)
+
+#     st.markdown("---")
+
+#     st.subheader("📊 System Evaluation")
+#     if st.button("Show Evaluation (Precision@5)"):
+#         from src.evaluation.evaluate import evaluate
+
+#         score = evaluate(num_samples=20, k=5)
+#         st.write(f"Average Precision@5: {score:.2f}")
